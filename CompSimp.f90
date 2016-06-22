8ad32ee5d3b2cd96982bd533b3aa4691e0d48b6c
whisker-rebellion <whisker-rebellion@users.noreply.github.com>
2016-06-21 19:51:02 -0500

    Add files via upload

---
+++ CompSimp.f90
@@ -0,0 +1,35 @@
+program CompSimp
+implicit none
+double precision x0,xn,x,h,Simp,exact,diff,f
+integer j,n,m
+write(*,*) 'enter n (must be even)'
+read(*,*) n
+m=n/2
+x0=0.d0
+xn=1.d0
+h=(xn-x0)/dfloat(n)
+Simp=f(x0)+f(xn)
+do j=1,m-1
+  x=x0+dfloat(2*j)*h
+  Simp=Simp+2.d0*f(x)
+enddo
+do j=1,m
+  x=x0+dfloat(2*j-1)*h
+  Simp=Simp+4.d0*f(x)
+enddo
+Simp=h/3.d0 *Simp
+exact=0.528408
+diff=dabs(Simp-exact)
+write(*,*)'exact=',exact
+write(*,*)'Simp=',Simp
+write(*,*)'|Simp-exact|=',diff
+stop
+end program CompSimp
+
+!!!! 0.63629436111989061883446424291635313615100026
+
+function f(x)
+double precision x,f
+f=x**(-1/4) *dsin(x)
+return
+end function f
---
+++ CompTrap.f90
@@ -0,0 +1,28 @@
+program CompTrap
+implicit none
+double precision x0,xn,h,sum,f,exact,diff,xj
+integer n,j
+write(*,*)'enter n'
+read(*,*) n
+x0=0.0d0
+xn=2.d0
+h=(xn-x0)/dfloat(n)
+sum=f(x0)+f(xn)
+do j=1,n-1
+  xj=x0+dfloat(j)*h
+  sum=sum+2.d0*f(xj)
+enddo
+sum=h/2.d0*sum
+exact=dlog(1.5d0)
+write(*,*) 'exact=',exact
+diff=dabs(sum-exact)
+write(*,*) 'Trapezoidal approx=',sum
+write(*,*) 'diff=',diff
+stop
+end program CompTrap
+
+function f(x)
+double precision x,f
+f=1/(x+4)
+return
+end function f
